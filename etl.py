61517cd2e047670c01ec0cfd18abb8f042c1a406
JoseMancera <jose.mancera@beekeeper.io>
2021-04-11 17:30:50 +0200

    First draft completed project

--- etl.py
+++ etl.py
@@ -7,37 +7,39 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
+    song_data = df.loc[:,['song_id', 'title', 'artist_id', 'year', 'duration']].values[0].tolist()
     cur.execute(song_table_insert, song_data)
-    
+
     # insert artist record
-    artist_data = 
+    artist_data = df.loc[:,['artist_id', 'artist_name', 'artist_location','artist_latitude','artist_longitude']].values[0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df.page=='NextSong']
 
     # convert timestamp column to datetime
-    t = 
-    
+    t = pd.to_datetime(df['ts'], unit='ms')
+
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = (df['ts'].tolist(),t.dt.hour.values.tolist(), t.dt.day.values.tolist(),
+                 t.dt.week.values.tolist(),t.dt.month.values.tolist(), t.dt.year.values.tolist(),
+                 t.dt.weekday.values.tolist())
+    column_labels = ('Timestamp','hour', 'day','week','month','year','weekday')
+    time_df = pd.DataFrame(list(time_data), index = list(column_labels)).transpose()
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df.sort_values(by='ts', ascending=False).loc[:,['userId','firstName', 'lastName', 'gender','level']].drop_duplicates('userId')
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -45,18 +47,19 @@ def process_log_file(cur, filepath):
 
     # insert songplay records
     for index, row in df.iterrows():
-        
+
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
-        
+
         if results:
             songid, artistid = results
         else:
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (df.ts[index].item(), int(df.userId[index]), df.level[index], songid, artistid,
+                         df.sessionId[index].item(), df.location[index], df.userAgent[index])
         cur.execute(songplay_table_insert, songplay_data)
 
 
